a3a6aba635d6d19085afa226f15e4165db2e9212
Joestarfish <130308219+Joestarfish@users.noreply.github.com>
2024-03-03 00:15:06 -0500

    Update BedrockEconomyProvider to use AsyncAPI

--- src/DaPigGuy/libPiggyEconomy/providers/BedrockEconomyProvider.php
+++ src/DaPigGuy/libPiggyEconomy/providers/BedrockEconomyProvider.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace DaPigGuy\libPiggyEconomy\providers;
 
-use cooldogedev\BedrockEconomy\api\legacy\ClosureContext;
+use cooldogedev\BedrockEconomy\api\type\AsyncAPI;
 use cooldogedev\BedrockEconomy\BedrockEconomy;
 use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
-use cooldogedev\BedrockEconomy\api\version\LegacyBEAPI;
-use cooldogedev\BedrockEconomy\currency\CurrencyManager;
+use cooldogedev\BedrockEconomy\currency\Currency;
+use cooldogedev\BedrockEconomy\libs\SOFe\AwaitGenerator\Await;
 use pocketmine\player\Player;
 use pocketmine\Server;
 
 class BedrockEconomyProvider extends EconomyProvider
 {
-    private LegacyBEAPI $api;
-    private CurrencyManager $currency;
+    private AsyncAPI $api;
+    private Currency $currency;
 
     public static function checkDependencies(): bool
     {
@@ -24,32 +24,48 @@ class BedrockEconomyProvider extends EconomyProvider
 
     public function __construct()
     {
-        $this->api = BedrockEconomyAPI::legacy();
-        $this->currency = BedrockEconomy::getInstance()->getCurrencyManager();
+        $this->api = BedrockEconomyAPI::ASYNC();
+        $this->currency = BedrockEconomy::getInstance()->getCurrency();
     }
 
     public function getMonetaryUnit(): string
     {
-        return $this->currency->getSymbol();
+        return $this->currency->symbol;
     }
 
     public function getMoney(Player $player, callable $callback): void
     {
-        $this->api->getPlayerBalance($player->getName(), ClosureContext::create(fn(?int $balance) => $callback($balance ?? $this->currency->getDefaultBalance())));
+        Await::f2c(
+            fn () => yield from $this->api->get($player->getXuid(), $player->getName()),
+            fn ($result) => $callback($result['amount']),
+            fn () => $callback($this->currency->getDefaultBalance())
+        );
     }
 
     public function giveMoney(Player $player, float $amount, ?callable $callback = null): void
     {
-        $this->api->addToPlayerBalance($player->getName(), (int)$amount, $callback ? ClosureContext::create($callback) : null);
+        Await::f2c(
+            fn () => yield from $this->api->add($player->getXuid(), $player->getName(), (int)$amount, 0),
+            $callback,
+            fn () => $callback ? $callback(false) : null
+        );
     }
 
     public function takeMoney(Player $player, float $amount, ?callable $callback = null): void
     {
-        $this->api->subtractFromPlayerBalance($player->getName(), (int)$amount, $callback ? ClosureContext::create($callback) : null);
+        Await::f2c(
+            fn () => yield from $this->api->subtract($player->getXuid(), $player->getName(), (int)$amount, 0),
+            $callback,
+            fn () => $callback ? $callback(false) : null
+        );
     }
 
     public function setMoney(Player $player, float $amount, ?callable $callback = null): void
     {
-        $this->api->setPlayerBalance($player->getName(), (int)$amount, $callback ? ClosureContext::create($callback) : null);
+        Await::f2c(
+            fn () => yield from $this->api->set($player->getXuid(), $player->getName(), (int)$amount, 0),
+            $callback,
+            fn () => $callback ? $callback(false) : null
+        );
     }
 }
